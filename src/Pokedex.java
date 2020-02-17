f9922cade7b10a4ab6110f38cd1c36c07afa77a0
David G <david.giasson@claurendeau.qc.ca>
2020-02-17 07:37:30 -0500

    Ajout du constructeur manquant dans Pokedex

--- src/Pokedex.java
+++ src/Pokedex.java
@@ -2,6 +2,11 @@ public class Pokedex {
     private String region;
     private Pokemon[] pokemons;
 
+    public Pokedex(String region, int nbPokemons) {
+        this.region = region;
+        this.pokemons = new Pokemon[nbPokemons + 1]; // '+1' pour reserver l'index 0
+    }
+
     public void ajouter(Pokemon pokemon) {
         pokemons[pokemon.getNumero()] = pokemon;
     }
