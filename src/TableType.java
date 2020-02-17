19b8dd321f23e84bda55ccc026ee99de5788c4a9
David G <david.giasson@claurendeau.qc.ca>
2020-02-17 07:36:54 -0500

    Ajout de la table des types

---
+++ src/TableType.java
@@ -0,0 +1,7 @@
+public class TableType {
+    private final static String[] types = {"<Missing>", "Bug", "Dragon", "Ice",
+            "Fighting", "Fire", "Flying", "Grass", "Ghost", "Ground",
+            "Electric", "Normal", "Poison", "Psychic", "Rock", "Water"};
+
+
+}
