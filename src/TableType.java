4a07e794cfbbd69330471150ce74688530b148e9
David G <david.giasson@claurendeau.qc.ca>
2020-02-18 02:13:31 -0500

    Traduction des types

--- src/TableType.java
+++ src/TableType.java
@@ -1,18 +1,19 @@
 public class TableType {
-    private final static String[] types = {"<Missing>", "Bug", "Dragon", "Electric",
-             "Ice", "Fighting", "Fire", "Flying", "Grass", "Ghost", "Ground",
-            "Normal", "Poison", "Psychic", "Rock", "Water"};
+    private final static String[] TYPES = {"<Inconnu>",
+            "Combat", "Dragon", "Eau", "Electrique", "Feu",
+            "Glace", "Insecte", "Normal",  "Plante", "Poison",
+            "Psychique", "Roche", "Sol", "Spectre", "Vol"};
 
     public static String getType(int idType) {
-        return types[idType];
+        return TYPES[idType];
     }
 
     public int[] getFaiblesses(int idType) {
-        // TODO Version temporaire, a completer si vous voulez...
+        //TODO Version temporaire, a completer si vous voulez...
         int[] faiblesses = new int[3];
-        faiblesses[0] = (idType + 1) % types.length;
-        faiblesses[1] = (idType + 3) % types.length;
-        faiblesses[2] = (idType + 6) % types.length;
+        faiblesses[0] = (idType + 1) % TYPES.length;
+        faiblesses[1] = (idType + 3) % TYPES.length;
+        faiblesses[2] = (idType + 6) % TYPES.length;
         return faiblesses;
     }
 }
