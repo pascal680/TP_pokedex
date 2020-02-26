22354a134bc556803c4d315d134897a3ab41393c
David G <david.giasson@claurendeau.qc.ca>
2020-02-26 07:58:54 -0500

    Ajout des tests d'ajout

--- src/Test.java
+++ src/Test.java
@@ -4,22 +4,38 @@ public class Test {
 
     public static void main(String[] args) {
         Test t = new Test();
-        if (t.reset())
-            System.out.println("Tests reussis!");
+
+        if (t.testAjouter() || t.testAjouterEvolution()) {
+            System.out.println("Tests d'ajout echoues!");
+            return;
+        }
+
+        System.out.println("Tous les tests reussis!");
     }
 
-    private boolean reset() {
+    public Test() {
         dex = new Pokedex();
-        return testAjouter();
     }
 
-    private boolean testAjouter() {
+    public boolean testAjouter() {
         if (dex.ajouter("Bulbasaur", 1, "Plante", "Poison") == null)
-            return false;
+            return true;
         if (dex.ajouter("Squirtle", 7, "Eau") == null)
-            return false;
-        if (dex.ajouter("Charmander", 4, "Feu") == null)
-            return false;
-        return true;
+            return true;
+        p = dex.ajouter("Charmander", 4, "Feu");
+        if (p == null)
+            return true;
+        return false;
     }
+
+    public boolean testAjouterEvolution() {
+        p = dex.ajouterEvolution(p, "Charmeleon", 5, "Feu");
+        if (p == null)
+            return true;
+        p = dex.ajouterEvolution(p, "Charizard", 6, "Feu", "Vol");
+        if (p == null)
+            return true;
+        return false;
+    }
+    
 }
